9636413c9d8f4dddf37606075834389d3351fd0a
Toplak Filip <blueplastic1@hotmail.com>
2021-08-05 15:04:04 +0200

    dinamicko dodavanje u listu, filtriranje, uvjeti

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import React, { useState } from 'react';
+
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense';
 
+const DUMMY_EXPENSES = [
+  { id: "1", title: "Ivo", amount: 294.67, date: new Date(2021, 4, 28) },
+  { id: "2", title: "Pero", amount: 66.6, date: new Date(2021, 6, 28) },
+  { id: "3", title: "Karlo", amount: 2314, date: new Date(2021, 2, 28) },
+  { id: "4", title: "Duro", amount: 111.1111, date: new Date(2021, 11, 28) },
+];
+
+
 function App() {
-  const expenses = [
-    { id: "1", title: "Ivo", amount: 294.67, date: new Date(2021, 4, 28) },
-    { id: "2", title: "Pero", amount: 66.6, date: new Date(2021, 6, 28) },
-    { id: "3", title: "Karlo", amount: 2314, date: new Date(2021, 2, 28) },
-    { id: "4", title: "Duro", amount: 111.1111, date: new Date(2021, 11, 28) },
-  ];
+const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-      <NewExpense/>
-      <Expenses items={expenses}></Expenses>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,11 +1,41 @@
-import "./NewExpense.css";
+import React, { useState } from 'react';
+
+import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 
-function NewExpense() {
+function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new Expense</button>
+      )}
+
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 }
